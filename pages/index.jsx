3a793414d826e4aeda93a4297cccf87a032bd091
arbaaz-77 <43441056+arbaaz-77@users.noreply.github.com>
2022-06-28 20:01:06 +0300

    added api key

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Weather 101 | Arbaaz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
