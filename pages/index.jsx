cdc880ae3b624534e37584d4eaaf4b031d75ce95
arbaaz-77 <43441056+arbaaz-77@users.noreply.github.com>
2022-07-02 00:41:45 +0300

    added weather display and loader

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import Widget from "../components/Widget";
+import Spinner from "../components/Spinner";
 
 const Home = () => {
   const [city, setCity] = useState("");
@@ -17,7 +19,6 @@ const Home = () => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setWeather(data);
       });
 
@@ -25,47 +26,52 @@ const Home = () => {
     setLoading(false);
   };
 
-  return (
-    <>
-      <Head>
-        <title>Weather 101 | Arbaaz</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <>
+        <Head>
+          <title>Weather 101 | Arbaaz</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      {/* Overlay */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-10" />
+        {/* Overlay */}
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/30 z-[1]" />
 
-      {/* BG  Image */}
-      <Image
-        src="https://images.unsplash.com/photo-1580193769210-b8d1c049a7d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
-        layout="fill"
-        className="object-cover"
-      />
+        {/* BG  Image */}
+        <Image
+          src="https://images.unsplash.com/photo-1580193769210-b8d1c049a7d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
+          layout="fill"
+          className="object-cover"
+        />
 
-      {/* Search */}
-      <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-10 text-white z-20">
-        <form
-          onSubmit={fetchData}
-          className="flex justify-between items-center w-full m-auto  p-3 bg-transparent border border-gray-300 text-white rounded-2xl"
-        >
-          <div>
-            <input
-              onChange={(e) => setCity(e.target.value)}
-              value={city}
-              className="bg-transparent border-none text-white focus:outline-none text-3xl"
-              type="text"
-              placeholder="Search City"
-            />
-          </div>
-          <button onClick={fetchData}>
-            <BsSearch size={20} />
-          </button>
-        </form>
-      </div>
+        {/* Search */}
+        <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-10 text-white z-20">
+          <form
+            onSubmit={fetchData}
+            className="flex justify-between items-center w-full m-auto  p-3 bg-transparent border border-gray-300 text-white rounded-2xl"
+          >
+            <div>
+              <input
+                onChange={(e) => setCity(e.target.value)}
+                value={city}
+                className="bg-transparent border-none text-white focus:outline-none text-3xl"
+                type="text"
+                placeholder="Search City"
+              />
+            </div>
+            <button onClick={fetchData}>
+              <BsSearch size={20} />
+            </button>
+          </form>
+        </div>
 
-      {/* Weather Display */}
-    </>
-  );
+        {/* Weather Display */}
+        {weather.main && <Widget data={weather} />}
+      </>
+    );
+  }
 };
 
 export default Home;
